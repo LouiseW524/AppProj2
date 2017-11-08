e6e7e7ecf73c71c3f4df422a4ce4fac1a5c70702
Conor Twomey <conor.twomey1@mycit.ie>
2017-11-08 10:22:16 +0000

    Correct Indentation

--- src/main/java/com/example/DataLoaders/DataLoader.java
+++ src/main/java/com/example/DataLoaders/DataLoader.java
@@ -14,13 +14,13 @@ import com.example.repositories.PersonRepo;
 @Component // ensures this is run because it is a bean
 public class DataLoader implements ApplicationRunner
 {
-@Autowired // Find a PersonRepo bean and autowire it into personRepo
-PersonRepo personRepo;
-@Override
-public void run(ApplicationArguments arg0) throws Exception
-{
-personRepo.save(new Person(1,"Minnie Mouse", 65, "minnie@cit.ie"));
-personRepo.save(new Person(2,"Daisy Duck", 50, "daisy@cit.ie"));
-personRepo.save(new Person(3,"Gru", 56, "gru@cit.ie"));
-}
+	@Autowired // Find a PersonRepo bean and autowire it into personRepo
+	PersonRepo personRepo;
+	@Override
+	public void run(ApplicationArguments arg0) throws Exception
+	{
+		personRepo.save(new Person(1,"Minnie Mouse", 65, "minnie@cit.ie"));
+		personRepo.save(new Person(2,"Daisy Duck", 50, "daisy@cit.ie"));
+		personRepo.save(new Person(3,"Gru", 56, "gru@cit.ie"));
+	}
 }
