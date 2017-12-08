fc0fedea44ec9443fc4f00ad9f3536a5737d96dd
Conor Twomey <conor.twomey1@mycit.ie>
2017-12-08 12:06:04 +0000

    Remove IDs and reformat DataLoader

--- src/main/java/com/example/DataLoaders/DataLoader.java
+++ src/main/java/com/example/DataLoaders/DataLoader.java
@@ -15,8 +15,10 @@ import com.example.entities.Item;
 import com.example.entities.Person;
 import com.example.enums.Category;
 import com.example.repositories.EventPackageRepo;
+import com.example.repositories.FlowerInventoryRepo;
 import com.example.repositories.FlowerRepo;
 import com.example.repositories.PersonRepo;
+import com.example.rest.entities.FlowerInventory;
 
 //ApplicationRunner is run before after the beans have been created and the application
 //context has been completed.
@@ -30,57 +32,65 @@ public class DataLoader implements ApplicationRunner
 	FlowerRepo flowerRepo;
 	@Autowired
 	EventPackageRepo eventPackageRepo;
+	@Autowired
+	FlowerInventoryRepo flowerInventoryRepo;
 	
 	
 	@Override
 	public void run(ApplicationArguments arg0) throws Exception
 	{
-		Person person = new Person(1,"Minnie Mouse", 65, "minnie@cit.ie");
-		Person person2 = new Person(2,"Daisy Duck", 50, "daisy@cit.ie");
-		Person person3 = new Person(3,"Gru", 56, "gru@cit.ie");
+		Person minnieMouse = new Person("Minnie Mouse", 65, "minnie@cit.ie");
+		Person daisyDuck = new Person("Daisy Duck", 50, "daisy@cit.ie");
+		Person gru = new Person("Gru", 56, "gru@cit.ie");
 		
-		Card card = new Card("Douglas", "Mary", person, "Hi", "Bye");
-		Card card2 = new Card("CIT", "Conor", person2, "Hi", "Bye");
-		Card card3 = new Card("Mahon", "Louise", person3, "Hi", "Bye");
+		Card card = new Card("Douglas", "Mary", minnieMouse, "Hi", "Bye");
+		Card card2 = new Card("CIT", "Conor", daisyDuck, "Hi", "Bye");
+		Card card3 = new Card("Mahon", "Louise", gru, "Hi", "Bye");
 		
-		Flower flower = new Flower(1, "Daffodil", 1.50, Category.FLOWER, "Yellow");
-		Flower flower2 = new Flower(2, "Rose", 15.00, Category.FLOWER, "Red");
-		Flower flower3 = new Flower(3, "Daisy", 3.50, Category.FLOWER, "White and Yellow");
+		Flower daffodil = new Flower(25, "Daffodil", 1.50, Category.FLOWER, "Yellow");
+		Flower rose = new Flower(50, "Rose", 15.00, Category.FLOWER, "Red");
+		Flower daisy = new Flower(75, "Daisy", 3.50, Category.FLOWER, "White and Yellow");
 		
-		ArrayList<Flower> bouquetFlowers = new ArrayList<Flower>();
-		ArrayList<Flower> bouquetFlowers2 = new ArrayList<Flower>();
-		ArrayList<Flower> bouquetFlowers3 = new ArrayList<Flower>();
+		ArrayList<Flower> bouquetDaffodils = new ArrayList<Flower>();
+		ArrayList<Flower> bouquetRoses = new ArrayList<Flower>();
+		ArrayList<Flower> bouquetDaisies = new ArrayList<Flower>();
 
-		bouquetFlowers.add(flower);
-		bouquetFlowers2.add(flower2);
-		bouquetFlowers3.add(flower3);
+		bouquetDaffodils.add(daffodil);
+		bouquetRoses.add(rose);
+		bouquetDaisies.add(daisy);
 		
 		ArrayList<Item> packageItems = new ArrayList<Item>();
 		ArrayList<Item> packageItems2 = new ArrayList<Item>();
 		ArrayList<Item> packageItems3 = new ArrayList<Item>();
 		
-		packageItems.add(flower);
-		packageItems2.add(flower2);
-		packageItems3.add(flower3);
+		packageItems.add(daffodil);
+		packageItems2.add(rose);
+		packageItems3.add(daisy);
 
-		Bouquet bouquet = new Bouquet(bouquetFlowers, card);
-		Bouquet bouquet2 = new Bouquet(bouquetFlowers2, card2);
-		Bouquet bouquet3 = new Bouquet(bouquetFlowers3, card3);
+		Bouquet bouquet = new Bouquet(bouquetDaffodils, card);
+		Bouquet bouquet2 = new Bouquet(bouquetRoses, card2);
+		Bouquet bouquet3 = new Bouquet(bouquetDaisies, card3);
 
 		
-		EventPackage eventPackage = new EventPackage( 1, packageItems, bouquet);
-		EventPackage eventPackage2 = new EventPackage( 2, packageItems2, bouquet2);
-		EventPackage eventPackage3 = new EventPackage( 3, packageItems3, bouquet3);
+		EventPackage eventPackage = new EventPackage(packageItems, bouquet);
+		EventPackage eventPackage2 = new EventPackage(packageItems2, bouquet2);
+		EventPackage eventPackage3 = new EventPackage(packageItems3, bouquet3);
+		
+		FlowerInventory flowerInventory = new FlowerInventory();
+		flowerInventory.addFlower(daffodil);
+		flowerInventory.addFlower(rose);
+		flowerInventory.addFlower(daisy);
 
-		personRepo.save(person);
-		personRepo.save(person2);
-		personRepo.save(person3);
-		flowerRepo.save(flower);
-		flowerRepo.save(flower2);
-		flowerRepo.save(flower3);
+		personRepo.save(minnieMouse);
+		personRepo.save(daisyDuck);
+		personRepo.save(gru);
+		flowerRepo.save(daffodil);
+		flowerRepo.save(rose);
+		flowerRepo.save(daisy);
 		eventPackageRepo.save(eventPackage);
 		eventPackageRepo.save(eventPackage2);
 		eventPackageRepo.save(eventPackage3);
+		flowerInventoryRepo.save(flowerInventory);
 
 	}
 }
