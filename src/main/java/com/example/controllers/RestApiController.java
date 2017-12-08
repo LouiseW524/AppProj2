ee1f3d94295033bdc24119cd541a1a2c20143995
Conor Twomey <conor.twomey1@mycit.ie>
2017-12-08 14:01:52 +0000

    Add flowerInventory response to REST API

--- src/main/java/com/example/controllers/RestApiController.java
+++ src/main/java/com/example/controllers/RestApiController.java
@@ -7,17 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entities.Flower;
+import com.example.repositories.FlowerInventoryRepo;
 import com.example.repositories.FlowerRepo;
+import com.example.rest.entities.FlowerInventory;
 
 @RestController
 public class RestApiController {
 	
 	@Autowired
 	FlowerRepo flowerRepo;
+	@Autowired
+	FlowerInventoryRepo flowerInventoryRepo;
 	
 	private final String REST_DIR = "/rest/";
 	private final String GET_FLOWER = REST_DIR + "getFlower";
 	private final String GET_ALL_FLOWERS = REST_DIR + "getAllFlowers";
+	private final String GET_FLOWER_INVENTORY = REST_DIR + "getFlowerInventory";
 	
 	@RequestMapping(GET_FLOWER)
 	public Flower getFlower(Long id) {
@@ -29,6 +34,12 @@ public class RestApiController {
 	public List<Flower> getAllFlowers() {
 		List<Flower> flowers = flowerRepo.findAll();
 		return flowers;
+	}
+	
+	@RequestMapping(GET_FLOWER_INVENTORY)
+	public FlowerInventory getFlowerInventory(Long id) {
+		FlowerInventory flowerInventory = flowerInventoryRepo.findOne(id.intValue());
+		return flowerInventory;
 	}
 	
 
