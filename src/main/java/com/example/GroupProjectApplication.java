0c3051ddd97d54f0a9cf362afcc9ba1ac9b0abb7
Conor Twomey <conor.twomey1@mycit.ie>
2017-12-08 13:23:30 +0000

    Autorun mongo script at startup

--- src/main/java/com/example/GroupProjectApplication.java
+++ src/main/java/com/example/GroupProjectApplication.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GroupProjectApplication {
 
 	public static void main(String[] args) {
+		try {
+			Runtime.getRuntime().exec("cmd /c start \"\" start_mongo.bat");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		SpringApplication.run(GroupProjectApplication.class, args);
 	}
 }
