6c7527dba6cd0ddbb2188594381677b21bf4848b
Conor Twomey <conor.twomey1@mycit.ie>
2017-12-08 12:22:11 +0000

    Add rest API package

---
+++ src/main/java/com/example/rest/entities/FlowerInventory.java
@@ -0,0 +1,61 @@
+package com.example.rest.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.entities.Flower;
+
+public class FlowerInventory {
+	
+	private Long id;
+	private static long nextId = 1;
+	private ArrayList<Flower> flowers;
+	
+	public void addFlower(Flower flower) {
+		flowers.add(flower);
+	}
+	
+	public void removeFlower(Flower flower) {
+		int index = 0;
+		for (index = 0; index < flowers.size(); index++) {
+			if (flowers.get(index) == flower) {
+				flowers.remove(index);
+				break;
+			}
+		}		
+	}
+	
+	public ArrayList<Flower> getFlowers() {
+		return flowers;
+	}
+	
+	public void setFlowers(ArrayList<Flower> flowers) {
+		this.flowers = flowers;
+	}
+	
+	private void setNewId() {
+		setId(nextId);
+		nextId++;
+	}
+
+	public FlowerInventory(ArrayList<Flower> flowers, ArrayList<Integer> quantity) {
+		super();
+		setNewId();
+		this.flowers = flowers;
+	}
+	
+	public FlowerInventory() {
+		super();
+		setNewId();
+		flowers = new ArrayList<Flower>();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+}
